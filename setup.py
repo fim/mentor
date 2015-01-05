01812ba58f4f8eaef853af203b4228931d03e0c2
Seraphim Mellos <seraphim.mellos@fcpl.com>
2015-01-05 11:13:07 +0000

    Update setup.py to install all dependencies by default

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(name='mentor',
     version=0.1,
     description='Simple file sharing tool over HTTP/HTTPS',
     author='fim',
+    install_requires=[
+        'M2Crypto',
+        'miniupnpc',
+        'gevent'],
 #    author_email='',
     scripts=['mentor']
 )
